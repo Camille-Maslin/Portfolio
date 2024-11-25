c2cafbe367f2c348b61837151c4992e8d1d2f5c1
Camille Maslin <65364408+MMillle@users.noreply.github.com>
2024-11-25 18:31:38 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import './styles/main.scss'
-import { initTheme } from './scripts/modules/theme.js'
-import { initScroll } from './scripts/modules/scroll.js'
-import { initNavigation } from './scripts/modules/navigation.js'
-import { initLanguage } from './scripts/modules/language.js'
+import './modules/theme.js';
+import './modules/navigation.js';
+import './modules/language.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initTheme()
