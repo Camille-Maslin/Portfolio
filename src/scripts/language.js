41d902202661167d9d210b32ad5146e5dfe6a188
MMillle <65364408+MMillle@users.noreply.github.com>
2024-11-25 19:32:15 +0000

    Deploying to gh-pages from @ MMillle/Portfolio@6d171463f36fd73c6ad87a63e3aa4d5e14322b69 🚀

--- src/scripts/language.js
+++ src/scripts/language.js
@@ -12,12 +12,9 @@ export const initLanguage = () => {
 
   const setLanguage = async (lang) => {
     try {
-      const path = `${window.location.origin}/Portfolio/locales/${lang}.json`;
-      console.log('Chemin complet:', new URL(path, window.location.href).href);
-      
-      const response = await fetch(path);
+      const response = await fetch(`locales/${lang}.json`);
       if (!response.ok) {
-        throw new Error(`HTTP Error! status: ${response.status}, path: ${path}`);
+        throw new Error(`HTTP Error! status: ${response.status}, path: ${response.url}`);
       }
       
       const translations = await response.json();
